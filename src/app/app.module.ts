5a20495fa6f01eb896b92967129183dd1fca359a
sanjana awachat <you@example.com>
2025-02-24 20:58:16 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule,MatSidenavModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,22 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  isSidebarOpen = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
   }
-  isBarVisible = false; // Initially hidden
-
-  toggleBar() {
-    this.isBarVisible = !this.isBarVisible;
-    
-  }
-  toggleBarclose(){
-    
-  }
-  
 }
